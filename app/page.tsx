8ad6655a3eaf3159fdba2243d7f3c68a8b8b30dc
RasyaIskandar <rasya.iskandar79@gmail.com>
2025-08-02 15:42:25 +0700

    update: perbaikan ESLint error & animasi blur

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,12 @@
 
 import { SmoothCursor } from "@/components/ui/smooth-cursor";
 import Intro from "@/components/Intro";
-import Hyperspeed from "@/components/file/Hyperspeed/Hyperspeed";
 
 export default function Page() {
   return (
     <Intro>
     <SmoothCursor />
-     <div className="relative bg-[#0A030F] w-screen h-screen overflow-hidden">
-        <Hyperspeed />
+      <div className="relative bg-[#0A030F] w-100% h-screen">
       </div>
     </Intro>
   );
